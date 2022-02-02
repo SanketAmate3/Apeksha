40387319845d168aa00f02186da2eca49a3a7781
Saheb <Saheb@LAPTOP-9KJKCCB1>
2022-02-02 22:52:30 +0530

    sdfsf

--- ApekshA/src/test/java/AS/A.java
+++ ApekshA/src/test/java/AS/A.java
@@ -2,7 +2,12 @@ package AS;
 
 public class A {
 
+	void m()
+	{
+		System.out.println("method m");
+	}
 	public static void main(String[] args) {
+		System.out.println("hi");
 		System.out.println("hi");
 
 	}
